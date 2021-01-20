ab4d9b84ac42288b6eb3a283996e4f40ca3929b9
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-20 22:25:57 +0100

    Added main

---
+++ leetcode_csharp/leetcode_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode_csharp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            char x = ThreeightnineFindTheDifference.FindTheDifference("aba", "abba");
+            char y = ThreeightnineFindTheDifference.FindTheDifference("abcd", "abcde");
+            char z = ThreeightnineFindTheDifference.FindTheDifference("", "y");
+            char w = ThreeightnineFindTheDifference.FindTheDifference("a", "a");
+            char q = ThreeightnineFindTheDifference.FindTheDifference("ae", "aea");
+            Console.WriteLine($"{x}  {y}   {z}   {w}    {q}");
+        }
+        
+    }
+}
